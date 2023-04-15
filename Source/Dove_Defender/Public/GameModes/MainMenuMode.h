6bafc2c4b8817acd56c503e0047700dbe020353f
Jairo Manon <jairomanon77@gmail.com>
2023-04-15 10:25:46 -0400

    Added all the classes needed for this milestone

---
+++ Source/Dove_Defender/Public/GameModes/MainMenuMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MainMenuMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DOVE_DEFENDER_API AMainMenuMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Dove_Defender/Public/Widgets/MyButton.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MyButton.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DOVE_DEFENDER_API UMyButton : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
